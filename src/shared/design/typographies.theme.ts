94f06237d501e0f3e1b3dfda3ace15a2c20dad18
Max Milan <max.oliveira@yazo.com.br>
2024-04-30 17:35:42 -0300

    fix: typographies size

--- src/shared/design/typographies.theme.ts
+++ src/shared/design/typographies.theme.ts
@@ -4,64 +4,64 @@ export const typographies: ITypographies = {
   bai: {
     regular: {
       family: "Bai Jamjuree",
-      size: "16px",
+      size: "14px",
       weight: "regular",
     },
     regularBold: {
       family: "Bai Jamjuree",
-      size: "16px",
+      size: "14px",
       weight: "bold",
     },
     subTitle: {
       family: "Bai Jamjuree",
-      size: "24px",
+      size: "16px",
       weight: "regular",
     },
     title: {
       family: "Bai Jamjuree",
-      size: "24px",
+      size: "16px",
       weight: "bold",
     },
     extraLarge: {
       family: "Bai Jamjuree",
-      size: "32px",
+      size: "24px",
       weight: "bold",
     },
     huge: {
       family: "Bai Jamjuree",
-      size: "48px",
+      size: "32px",
       weight: "bold",
     },
   },
   ubuntu: {
     regular: {
       family: "Ubuntu",
-      size: "16px",
+      size: "14px",
       weight: "regular",
     },
     regularBold: {
       family: "Ubuntu",
-      size: "16px",
+      size: "14px",
       weight: "bold",
     },
     subTitle: {
       family: "Ubuntu",
-      size: "24px",
+      size: "16px",
       weight: "regular",
     },
     title: {
       family: "Ubuntu",
-      size: "24px",
+      size: "16px",
       weight: "bold",
     },
     extraLarge: {
       family: "Ubuntu",
-      size: "32px",
+      size: "24px",
       weight: "bold",
     },
     huge: {
       family: "Ubuntu",
-      size: "48px",
+      size: "32px",
       weight: "bold",
     },
   },
